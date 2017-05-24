5e7fe86fc010dafa5ea446d4a77bc93616ebf08a
fechhe <fechhe@gmail.com>
2017-05-24 17:51:04 -0400

    recomposicion de working tree y adicion de app

---
+++ PAGE/www/js/app/controllers.js
@@ -0,0 +1,62 @@
+angular.module('Cartilla.controllers',['uiGmapgoogle-maps','Cartilla.services','ngTable'])
+
+	.controller('mainCtrl', function($scope,parserSrv,$http,$filter,NgTableParams,$state) {
+	
+		$scope.clinicas =null;
+	
+		$scope.getClinicas = function(){	
+				localidad = 'CAPITAL FEDERAL';
+				especialidad = 'Clinico';
+			
+				$http.post( "http://www.gestionarturnos.com/Fede/listarClinicasAll.php", {'localidad':localidad,'especialidad':especialidad})
+				.success(function(response) {
+					var parseado;
+					
+					console.log(response);
+					if (response===""){ //VERIFICAMOS QUE EXISTAN CLINICAS PARA PARSEAR
+						parseado=[];
+					}
+					else
+					{	
+						parseado = parserSrv.parseMarkers(response);
+					}
+					
+          $scope.clinicas=parseado;
+					console.log("clinicas");
+					console.log($scope.clinicas);
+					
+				//PARAMETROS DE LA TABLA FILTRO Y SORTING
+					$scope.usersTable = new NgTableParams({
+          	page: 1,
+            count: 10
+          },
+					{
+            total: $scope.clinicas.length, 
+            getData: function (params) {
+							$scope.data = params.sorting() ? $filter('orderBy')($scope.clinicas, params.orderBy()) : $scope.clinicas;
+   						$scope.data = params.filter() ? $filter('filter')($scope.data, params.filter()) : $scope.data;
+							console.log("data");
+							console.log($scope.data);
+							parserSrv.setLatLongAvrg($scope.data);
+   						$scope.data = $scope.data.slice((params.page() - 1) * params.count(), params.page() * params.count());
+              return $scope.data;
+          	}
+  				});
+				})
+				
+				.error(function(response) {
+					alert("Hubo un error al conectarse, revise su conexion a internet, de otra forma intente luego");
+				})
+		}
+		
+		$scope.getClinicas();
+		
+		$scope.mostrarMapa=function(){
+			console.log(parserSrv.getLatitude());
+			console.log(parserSrv.getLongitude());
+			$scope.map = {center: {latitude: parserSrv.getLatitude(), longitude: parserSrv.getLongitude() }, zoom: 11, bounds: {}};
+			$scope.markers=$scope.data;
+			$state.go('mapa');
+		}
+		
+	})
---
+++ PAGE/www/js/app/services.js
@@ -0,0 +1,52 @@
+angular.module('Cartilla.services', [])
+
+.service('parserSrv', function(){
+  
+  var latCABA=-34.5991567;
+  var longCABA = -58.369587499999966 ;
+  var _lat;
+  var _long;
+  var _zoom
+
+    this.parseMarkers = function(array){
+       	array.map(function(elem){
+          elem.id =  elem.IDCLI;
+          delete elem["0"]; 
+          delete elem["1"];
+          delete elem["2"];
+          delete elem["3"];
+          delete elem["4"];
+          delete elem["5"];
+          delete elem["6"]; 
+        } );
+       	console.log(array);
+       	return array;
+    }
+    
+    this.setLatLongAvrg = function(obj){
+        _long = (obj.reduce(function(a, b) {return a + parseFloat(b.longitude);}, 0) / obj.length).toString();
+        _lat = (obj.reduce(function(a, b) {return a + parseFloat(b.latitude);}, 0) / obj.length).toString();
+    }
+    
+    this.getLatitude = function(){
+      if (isNaN(_lat)){
+         return latCABA
+      }
+      else
+       return _lat
+    }
+    
+    this.getLongitude = function(){
+      if(isNaN(_long)){
+        return longCABA
+      }
+      else{
+        return _long
+      }
+    }
+    
+    
+
+})
+
+
