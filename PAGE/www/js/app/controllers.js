622b53b42687b028b9acaff4bace0987733996a8
FedericoJoel <fechhe@gmail.com>
2017-07-23 22:43:55 -0300

    Cambios simples

--- PAGE/www/js/app/controllers.js
+++ PAGE/www/js/app/controllers.js
@@ -1,42 +1,40 @@
 angular.module('Cartilla.controllers',['uiGmapgoogle-maps','Cartilla.services','ngTable'])
 
 	.controller('mainCtrl', function($scope,parserSrv,$http,$filter,NgTableParams,$state) {
-	
+
 		$scope.clinicas =null;
-	
-		$scope.getClinicas = function(){	
+
+		$scope.getClinicas = function(){
 				localidad = 'CAPITAL FEDERAL';
 				especialidad = 'Clinico';
-			
+
 				$http.post( "http://www.gestionarturnos.com/Fede/listarClinicasAll.php", {'localidad':localidad,'especialidad':especialidad})
 				.success(function(response) {
 					var parseado;
-					
+
 					console.log(response);
 					if (response===""){ //VERIFICAMOS QUE EXISTAN CLINICAS PARA PARSEAR
 						parseado=[];
 					}
 					else
-					{	
+					{
 						parseado = parserSrv.parseMarkers(response);
 					}
-					
+
           $scope.clinicas=parseado;
 					console.log("clinicas");
 					console.log($scope.clinicas);
-					
+
 				//PARAMETROS DE LA TABLA FILTRO Y SORTING
 					$scope.usersTable = new NgTableParams({
           	page: 1,
             count: 10
           },
 					{
-            total: $scope.clinicas.length, 
+            total: $scope.clinicas.length,
             getData: function (params) {
 							$scope.data = params.sorting() ? $filter('orderBy')($scope.clinicas, params.orderBy()) : $scope.clinicas;
    						$scope.data = params.filter() ? $filter('filter')($scope.data, params.filter()) : $scope.data;
-							console.log("data");
-							console.log($scope.data);
 							parserSrv.setClinicas($scope.data);
 							parserSrv.setLatLongAvrg($scope.data);
    						$scope.data = $scope.data.slice((params.page() - 1) * params.count(), params.page() * params.count());
@@ -44,14 +42,14 @@ angular.module('Cartilla.controllers',['uiGmapgoogle-maps','Cartilla.services','
           	}
   				});
 				})
-				
+
 				.error(function(response) {
 					alert("Hubo un error al conectarse, revise su conexion a internet, de otra forma intente luego");
 				})
 		}
-		
+
 		$scope.getClinicas();
-		
+
 		$scope.mostrarMapa=function(){
 			console.log(parserSrv.getLatitude());
 			console.log(parserSrv.getLongitude());
@@ -59,5 +57,5 @@ angular.module('Cartilla.controllers',['uiGmapgoogle-maps','Cartilla.services','
 			$scope.markers = parserSrv.getClinicas();
 			$state.go('mapa');
 		}
-		
+
 	})
